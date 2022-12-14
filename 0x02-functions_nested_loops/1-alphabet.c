918355cd80d3d2f121f6051bc8d370ff9c6331f2
SurafelFikadu <surafelfikadu05@gmail.com>
2022-12-13 23:07:59 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry point
- *
- * Return: Always 0
+ * main - print_alphabet
+ * Return: 0
  */
 
 void print_alphabet(void)
-
 {
 	char c = 'a';
 
-	while (c <= '2')
+	while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
