55573e052daee5b6d5e52311f9226c4384540645
SurafelFikadu <surafelfikadu05@gmail.com>
2022-12-16 11:53:27 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+
+char d;
+
+c = 'a';
+d = 'A';
+while
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
+while
+(d <= 'Z')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
