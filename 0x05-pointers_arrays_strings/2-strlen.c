9befac88c70dd0adce0ca05231b3efa6e584e15d
SurafelFikadu <surafelfikadu05@gmail.com>
2022-12-20 06:31:39 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * _strlen - function to get the length of string
+ * _strlen -> function to get the length of string
  * @s: string pointer to passed to this function
  * Return: return length of the string
  */
