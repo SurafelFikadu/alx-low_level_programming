9a2826cb9891879d921dcc7488b1138d4620f42b
SurafelFikadu <surafelfikadu05@gmail.com>
2022-12-20 05:21:57 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function to get the length of string
+ * @s: string pointer to passed to this function
+ * Return: return length of the string
+ */
+
+int _streln(char *s)
+{
+return (strlen(s));
+}
