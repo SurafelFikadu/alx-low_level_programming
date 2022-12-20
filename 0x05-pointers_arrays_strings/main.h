ed7a387b97e591bef099e52cef0678ef918fd879
SurafelFikadu <surafelfikadu05@gmail.com>
2022-12-20 03:22:22 -0800

    task 1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c);
 void reset_to_98(int *n);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
