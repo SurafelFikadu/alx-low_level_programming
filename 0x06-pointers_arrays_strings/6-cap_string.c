a4536047bab3180e94675b922bdb571e59c16556
SurafelFikadu <surafelfikadu05@gmail.com>
2022-12-22 09:06:47 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,11 +24,12 @@ str[index - 1] == '.' ||
 str[index - 1] == '!' ||
 str[index - 1] == '?' ||
 str[index - 1] == '-' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
 str[index - 1] == '{' ||
 str[index - 1] == '}' ||
 index == 0)
+
 str[index] -= 32;
 
 index++;
