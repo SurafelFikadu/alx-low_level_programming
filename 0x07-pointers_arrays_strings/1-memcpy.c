6b482b4c71feefe7861cdf6597b7a2fe3824b68f
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-02 13:55:54 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** _memcpy - filles memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
+ * @n: length of buffer
+ * Return: new string.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
