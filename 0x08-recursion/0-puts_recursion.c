2000cc202707486fbb5373303cfb51f6fefcf598
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-04 06:18:01 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,19 @@
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: The string to be printed.
- * Return: Always 0.
+ * Return: out put
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 
 	else
+	{
 		_putchar('\n');
+	}
 }
