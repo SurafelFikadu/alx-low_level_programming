e3df81c903fc05a9ead12cb9f50e2a8ba704ed8c
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-04 08:04:18 -0800

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,21 +3,20 @@
 
 /**
  * factorial - Write a function that returns the factorial of a given number.
- * @n: If n is lower than 0, the function should return -1 to indicate an error.
- * Return: 0
+ * @n: The number to find the factorial of.
+ *
+ * Return: If n > 0 - the factoral of n.
+ *         If n < 0 - 1 to indicate an error.
+ *
  */
 
 int factorial(int n)
 {
-	int r;
+	int result = n;
 
-	r = factorial(1);
-	printf("%d\n", r);
-	r = factorial(5);
-	printf("%d\n", r);
-	r = factorial(10);
-	printf("%d\n", r);
-	r = factorial(-1024);
-	printf("%d\n", r);
-	return (0);
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
 }
