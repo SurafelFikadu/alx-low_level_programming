7ef2920afcae3b313919914ce21469009beccd7f
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-05 16:04:13 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
 
-	if (*s != '\0')
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + strlen recursion(s));
 }
