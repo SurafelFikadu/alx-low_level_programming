4f3934cf2381924b0c42c833fad6ff55567f5ae4
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-08 03:53:04 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,32 +3,31 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
+ * Return: returns to capitalized string.
  */
 char *cap_string(char *str)
 {
-int index = 0;
+	int index = 0;
 
-while (str[++index])
-{
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a' && str[index]x <= 'z'))
+			index++;
 
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '-' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}'}
-str[index] -= 32;
-}
-return (str);
+		if (str[index - 1] == ' ' ||
+				str[index -1] == '\t' ||
+				str[index -1] == '\n' ||
+				str[index -1] == ',' ||
+				str[index -1] == ';' ||
+				str[index -1] == '.' ||
+				str[index -1] == '!' ||
+				str[index -1] == '?' ||
+				str[index -1] == '"' ||
+				str[index -1] == '(' ||
+				str[index -1] == ')' ||
+				str[index -1] == '{' ||
+				str[index -1] == '}')
+			str[index] -= 32;
+	}
+	return (str);
 }
