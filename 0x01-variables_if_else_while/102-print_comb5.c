54efa3f8a1a844fa09adee161eb38bb1d95485b9
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-11 06:25:50 -0800

    uodated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
@@ -19,7 +19,7 @@ int main(void)
 				putchar(' ');
 				putchar(j / 10 + '0');
 				putchar(j % 10 + '0');
-				
+
 				if (i * 100 + j != 9899)
 				{
 					putchar(',');
