55d2be8befa051b8229d60695926e765f3ec725d
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-11 06:12:47 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,13 +12,14 @@ for (i = 0; i < 100; i++)
 {
 for (j = 0; i < 100; j++)
 {
-if (j < i)
+if (j > i)
 {
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
+
 if (i * 100 + j != 9899)
 {
 putchar(',');
@@ -28,5 +29,6 @@ putchar(' ');
 }
 }
 putchar('\n');
+
 return (0);
 }
