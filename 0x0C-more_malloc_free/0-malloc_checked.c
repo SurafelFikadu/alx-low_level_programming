ecd6a25a4d75398cc0c288b3d21845ec65bbc83c
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-12 04:02:19 -0800

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * malloc_checked - array for prints a string
+ * @b: number of memory
+ * Result: void
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
