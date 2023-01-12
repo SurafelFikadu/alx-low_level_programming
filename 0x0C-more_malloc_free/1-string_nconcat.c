9469bd3ebe14114fcf5614a636037050e93f9aee
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-12 05:45:17 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,15 +23,13 @@ size1++;
 }
 while (s2[size2] != '\0')
 {
-size1++;
-}
-while (s2[size2] != '\0')
-{
 size2++;
 }
+
 if (n > size2)
 n = size2;
 p = malloc((size1 + n + 1) * sizeof(char));
+
 if (p == NULL)
 return (0);
 for (i = 0; i < size1; i++)
