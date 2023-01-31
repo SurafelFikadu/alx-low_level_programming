b8bc3df535adfd5814320acba2a1ae9e63890deb
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-31 01:27:17 -0800

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -31,6 +31,14 @@ size_t looped_listint_len(const listint_t *head)
 			{
 				nodes++;
 				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = head->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
 			}
 			return (nodes);
 		}
