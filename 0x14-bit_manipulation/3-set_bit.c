63cfa068b234b335236e0b71c89f9995ec9c980f
SurafelFikadu <surafelfikadu05@gmail.com>
2023-02-03 04:15:37 -0800

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,11 +6,11 @@
  * @index: The index to set the value at - indices start at 0.
  *
  * Return: If an error occurs - -1.
- * Otherwise - 1.
+ *         Otherwise - 1.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 0))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	*n ^= (1 << index);
