83222f6f7932b9dbd12d1bdf00581b9dbab00f20
SurafelFikadu <surafelfikadu05@gmail.com>
2023-02-02 17:45:43 -0800

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: if big-endin - 0. if little-endin -1.
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}
